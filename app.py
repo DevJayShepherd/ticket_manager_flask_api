fe2928db49022e243b377a8377d3520c577ef6f0
Jay Shepherd <69314244+DevJayShepherd@users.noreply.github.com>
2021-11-17 20:19:42 +0000

    .env - barebones file added, in preparation of adding postgres (#2)
    .envexample - added for other developers to fill
    __init__.py - moved app to run from here with some config
    app.py - removed fast start from flask docs

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.get('/')
-def homepage():
-    return {'detail': 'Welcome to ticket manager API! Please Visit /docs for endpoints.'}  # TODO add url to docs
