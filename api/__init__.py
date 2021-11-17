2afb2304ee87f7aa5c02d445e4d85c3352bab3ff
JayShepherdDhani <jay.shepherd@dhani.com>
2021-11-17 20:51:29 +0000

    __init__.py - registered new blueprints events.py - setup blueprint and endpoints for event management tickets.py - setup blueprint and endpoints for ticket management

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask
 
+from api.events.events import events
+from api.tickets.tickets import tickets
 
 
 def create_app(test_config=None):
-
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
@@ -15,5 +16,8 @@ def create_app(test_config=None):
     @app.get('/')
     def homepage():
         return {'detail': 'Welcome to ticket manager API! Please Visit /docs for endpoints.'}  # TODO add url to docs
+
+    app.register_blueprint(events)
+    app.register_blueprint(tickets)
 
     return app
