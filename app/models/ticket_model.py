38f17ce3bbcf9f909e5e869e5210930ba1754acf
JayShepherdDhani <jay.shepherd@dhani.com>
2021-11-18 12:00:01 +0000

    built out endpoints with basic crud opertations and moved the folders around a bit to clean up structure

--- app/models/ticket_model.py
+++ app/models/ticket_model.py
@@ -1,7 +1,7 @@
 import random
 from datetime import datetime
 
-from api.core import db
+from app.settings import db
 
 
 class Ticket(db.Model):
@@ -11,24 +11,17 @@ class Ticket(db.Model):
     ticket_number = db.Column(db.Integer, nullable=False)
     status = db.Column(db.String(255), nullable=False)
     redeemed_at = db.Column(db.DateTime, onupdate=datetime.now(), nullable=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    event_id = db.Column(db.Integer,
+                         db.ForeignKey('events.id'))  # TODO Make bigint to store hashed number instead of raw
 
-    def generate_ticket_number(self):
-        random_number_one = str(random.randint(1, 5000))
-        random_number_two = str(random.randint(1, 5000))
+    @staticmethod
+    def generate_ticket_number():
+        random_number_one = str(random.randint(1, 300))
+        random_number_two = str(random.randint(1, 300))
         ticket_number_string = random_number_one + random_number_two
-        ticket_number = hash(ticket_number_string)
+        ticket_number = ticket_number_string  # TODO Hash the number and store to avoid duplicates
 
         return ticket_number
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self.ticket_number = self.generate_ticket_number()
-
     def __repr__(self) -> str:
         return 'Ticket>>> {self.ticket_number}'
-
-
-
-
